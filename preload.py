bc3adbe787484839f888192c1c5f028060c065bf
bsinger <benedikt.singer@ist.ac.at>
2022-08-08 15:36:41 +0200

    Preloading works

--- preload.py
+++ preload.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import mrcfile.utils as utils
 import mrcfile
+import os
 
 
 def mrc_to_png(path, contrast):
@@ -14,15 +15,18 @@ def mrc_to_png(path, contrast):
     )
     image = Image.fromarray(object, mode="I;16")
     image = image.resize((400, 400))
-    image.save(path.replace("mrc", "png"))
+    image.save("/".join(path.split("/")[:-1])+"/preload/"+path.split("/")[-1].replace("mrc","png"))
 
 
-def iterate_over_dir(input_dir):
+def iterate_over_dir(input_dir,contrast):
     """Iterates over the given input directory and calls for every MRC file in the folder the mrc to png function and stores it in a subfolder with the name "preloaded_micrographs" """
-    pass
+    if "preload" not in os.listdir(input_dir): os.mkdir(input_dir + "preload")
+    for file in os.listdir(input_dir):
+        if file.endswith("mrc"):
+            mrc_to_png(input_dir + file, contrast)
 
 
 if __name__ == "__main__":
-    path = "./example_data/8.mrc"
+    path = "./example_data/"
     # iterate over files of given directory and preload each file
-    mrc_to_png(path, 4)
+    iterate_over_dir(path, 4)
